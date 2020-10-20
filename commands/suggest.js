78fb916e7d874a68a25b1d83c5a59c9c7aa89e3d
raynesworld <73146731+raynesworld@users.noreply.github.com>
2020-10-19 19:40:12 -0500

    Create suggest.js

---
+++ commands/suggest.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+module.exports = {
+	name: 'suggest',
+	description: 'Logs server suggestions to a suggestions channel and allows reactions.',
+	usage: '[suggestion]',
+	aliases: ['suggestion'],
+	async execute(message, args) {
+
+	let suggestion = args.join(" ");
+  if(!suggestion) return;
+
+	let sugEmbed = new Discord.MessageEmbed()
+	.setTitle("Server Suggestion")
+	.setDescription(suggestion)
+	.setColor("#ffff00")
+  .setFooter(`Suggested by ${message.author.username}`)
+	.setTimestamp();
+
+	let sugChannel = message.guild.channels.cache.find(channel => channel.name === "suggestions");
+	if(!sugChannel) return message.author.send("Can't find channel.");
+
+  if(message.guild === null) return message.channel.send("Please send this in a server channel, not a DM!");
+
+	message.delete({timeout: 5000});
+	sugChannel.send(sugEmbed).then(async embedMessage => {
+		await embedMessage.react('✅');
+		await embedMessage.react('❌');
+	});
+
+	return;
+
+	},
+};
