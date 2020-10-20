234d17167cf586f4b31b9e74f3b2cea88881e567
raynesworld <73146731+raynesworld@users.noreply.github.com>
2020-10-19 19:40:37 -0500

    Create qotd.js

---
+++ commands/qotd.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: 'qotd',
+  category: 'fun',
+  description: 'Bot posts a question of the day.',
+  usage: '[question]',
+  execute(message, args) {
+
+  let qotd = args.join(" ");
+  if(!qotd) return;
+
+  let embed = new Discord.MessageEmbed()
+  .setTitle("Question of the Day")
+  .setDescription(qotd)
+  .setColor("#ffff00")
+  .setTimestamp();
+
+  let qotdChannel = message.client.channels.cache.get("634977506122006559");
+  if(!qotdChannel) return message.reply("Question of the Day channel could not be found.");
+
+try {
+  qotdChannel.send(embed);
+} catch(error) {
+  console.error(error);
+}
+  }
+}
