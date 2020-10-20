abb9c9c3e91e1c88e8f1ed422dd42cfa3518d84f
raynesworld <73146731+raynesworld@users.noreply.github.com>
2020-10-19 19:40:53 -0500

    Create flip.js

---
+++ commands/flip.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'flip',
+  category: 'fun',
+	description: 'Flips a coin',
+	usage: '[question]',
+  aliases: ['coinflip', 'coin'],
+	execute(message, args) {
+
+		var rand = ["Heads.", "Tails."];
+
+    let result = Math.floor(Math.random() * rand.length);
+
+		message.channel.send(rand[result]);
+
+}
+};
