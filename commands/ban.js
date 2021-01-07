d6b731e76ab3b3bde74022e2756e578bcbe2e90c
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-01-06 20:10:14 -0600

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,7 +3,7 @@ const { stripIndents } = require("common-tags");
 
 module.exports = {
 	name: 'ban',
-  category: 'moderation',
+ 	category: 'moderation',
 	description: 'Bans the user from the server.',
 	usage: '[user] [reason]',
 	async execute(message, args) {
@@ -12,26 +12,26 @@ module.exports = {
 	if(!user) return message.channel.send("Can't find user.");
 
 	let reason = args.slice(1).join(" ");
-  if(!reason) reason = "No reason provided.";
+  	if(!reason) reason = "No reason provided.";
 
 	if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Nice try, Pleb.");
 	if(user.hasPermission("BAN_MEMBERS")) return message.channel.send("That user is too powerful.");
 
 	let banEmbed = new Discord.MessageEmbed()
-  .setTitle("**User Banned**")
+  	.setTitle("**User Banned**")
 	.setColor("#bc0000")
-  .setTimestamp()
-  .setDescription(stripIndents`**Banned User:** ${user.displayName}
-  **Reason:** ${reason}
-  **Responsible Moderator:** ${message.author.username}`);
+  	.setTimestamp()
+	.setDescription(stripIndents`**Banned User:** ${user.displayName}
+	**Reason:** ${reason}
+	**Responsible Moderator:** ${message.author.username}`);
 
-	let banChannel = message.guild.channels.cache.find(channel => channel.name === "logs");
-	if(!banChannel) return message.channel.send("Can't find logs channel.");
+	let logs = message.guild.channels.cache.find(channel => channel.name === "logs");
+	if(!logs) return message.channel.send("Can't find logs channel.");
 
   try {
-	await message.delete({ timeout: 5000 });
+	await message.delete({ timeout: 1000 });
 	await user.ban({reason});
-	await banChannel.send(banEmbed);
+	await logs.send(banEmbed);
   }catch (e){
   console.log(e);
   }
