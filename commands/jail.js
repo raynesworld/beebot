3ead659059afd5b1780c0379c052c3b129a98fa6
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-01-06 20:11:31 -0600

    Update jail.js

--- commands/jail.js
+++ commands/jail.js
@@ -4,20 +4,20 @@ const { stripIndents } = require("common-tags");
 
 module.exports = {
 	name: 'jail',
-  category: 'moderation',
+  	category: 'moderation',
 	description: 'Removes all roles from a user except the restricted prisoner role',
 	usage: '[user, reason]',
 	async execute(message, args) {
 
-	let jUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-	if(!jUser) return message.channel.send("Couldn't find user.");
+	let user = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+	if(!user) return message.channel.send("Couldn't find user.");
 
   let reason = args.slice(1).join(" ");
   if(!reason) reason = "No reason provided.";
 
 	if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(`Nice try ${message.author.username}, but you do not have permission to jail someone.`);
 
-	if(jUser.hasPermission("MANAGE_ROLES")) return message.channel.send(`${jUser} is too powerful.`);
+	if(user.hasPermission("MANAGE_ROLES")) return message.channel.send(`${user} is too powerful.`);
 
   if(message.guild === null) return message.channel.send("Please send this in a server channel, not a DM!");
 
@@ -25,27 +25,27 @@ module.exports = {
 	.setTitle(`Jailed User`)
 	.setColor("#bc0000")
 	.setTimestamp()
-	.setDescription(stripIndents`**Jailed User:** ${jUser}
+	.setDescription(stripIndents`**Jailed User:** ${user}
 	**Reason:** ${reason}
 	**Jailed By:** ${message.author.username}`);
 
-	let jailchannel = message.guild.channels.cache.get("632769031199522826");
-	if(!jailchannel) return message.channel.send("Couldn't find logs channel.");
+	let logs = message.guild.channels.cache.get("632769031199522826");
+	if(!logs) return message.channel.send("Couldn't find logs channel.");
 
-	const roles = jUser.roles.cache.filter(r => r.id !== message.guild.id)
+	const roles = user.roles.cache.filter(r => r.id !== message.guild.id)
 
 		try {
-			await jUser.roles.remove(roles)
-			await jUser.roles.add('629180728306827264')
-			await jailchannel.send(jailEmbed)
+			await user.roles.remove(roles)
+			await user.roles.add('629180728306827264')
+			await logs.send(jailEmbed)
 			await message.delete({timeout: 2000});
 		} catch(e) {
 			console.error(e);
 		}
 
 	setTimeout(() => {
-		jUser.roles.remove('629180728306827264')
-		jUser.roles.set(roles);
+		user.roles.remove('629180728306827264')
+		user.roles.set(roles);
 	}, 86400000);
 }
 };
