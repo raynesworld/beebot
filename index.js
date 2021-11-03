ecf77a7a7b5e1c97cbe18fc07cc5a2cc6552e305
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-11-03 11:07:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require ('discord.js');
+const { Client, Intents } = require ('discord.js');
 const { prefix, token } = require('./config.json');
-const { Client, Intents } = require('discord.js');
-const myIntents = new Intents();
-myIntents.add('GUILD_PRESENCES', 'GUILD_MEMBERS');
-const client = new Client({ ws: { intents: Intents.ALL } });
-const stripIndents = require('common-tags');
+const client = new Discord.Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES],
+	partials: ["MESSAGE", "CHANNEL", "REACTION"]
+});
+const { stripIndents } = require('common-tags');
 const fs = require('fs');
+const ms = require('ms');
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 this.channels = new Discord.Collection();
@@ -17,15 +19,18 @@ for (const file of commandFiles) {
 			client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
+const blacklist = ["[BAD WORDS]"]
+
+client.on('ready', async () => {
     console.log('Ready!');
+		client.user.setActivity("for !help", { type: "WATCHING"});
 });
 
-client.on('message', message => {
+client.on('messageCreate', message => {
 
-	if(message.author.bot) return;
+if(message.author.bot) return;
 
-  if (message.content.startsWith(prefix)) {
+if(message.content.startsWith(prefix)) {
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
@@ -33,7 +38,8 @@ client.on('message', message => {
 	const command = client.commands.get(commandName)
 		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
- 	if (!command) return;
+ 	if(!command) return;
+	if(!args) return;
 
 try {
 	command.execute(message, args);
@@ -48,84 +54,111 @@ try {
 
  if(message.channel.id === "619965922903326742" || message.channel.id === "619965965068795904") {
     try {
-    message.react('❤️')
-                  .then(() => message.react('💔'));
-  } catch (error) {
-    console.log(error);
+    message.react('❤️').then(() => message.react('💔'));
+  } catch(e) {
+    console.log(e);
   };
  }; //Close dating channels react
+
+ //Blacklist Words
+for (i = 0; i < blacklist.length; i++) {
+          if (message.content.toLowerCase().includes(blacklist[i])) {
+try {
+	message.delete();
+	} catch (e) {
+		console.log(e);
+	};
+	}};
  }; //Close else if
- }); // Close message listener
+}); // Close message listener
 
-
-//Cache New Members
+//Cache New Members and give then "No Roles" Role
 client.on('guildMemberAdd', (member) => {
   client.users.fetch(member.id, true);
+	var role = member.guild.roles.cache.get('895505308187766785')
+	member.roles.add(role);
+});
+
+// Remove "No Roles" Role if they get Gender Role
+client.on("guildMemberUpdate", (oldMember, newMember) => {
+    if (newMember.roles.cache.some(role => role.name === "Female") || newMember.roles.cache.some(role => role.name === "Male")
+		|| newMember.roles.cache.some(role => role.name === "They/Them") ) {
+        newMember.roles.remove("895505308187766785");
+    }
+});
+
+//Send Welcome Message
+client.on("guildMemberAdd", (member) => {
+
+	client.users.fetch(member.id, true);
+
+	var welcomes = [
+		`A wild **${member.displayName}** has appeared!`,
+		`**${member.displayName}** just joined the party.`,
+		`Everyone welcome **${member.displayName}**!`,
+		`**${member.displayName}** just slid into the server.`,
+		`Good to see you, **${member.displayName}**.`,
+		`**${member.displayName}** has arrived.`,
+		`Welcome, **${member.displayName}**. Pull up a seat.`,
+		`We've been expecting you, **${member.displayName}**`,
+		`Did **${member.displayName}** just enter without knocking?`,
+		`Houston, we have a problem. **${member.displayName}** just joined.`,
+		`Roses are red. Violets are blue. **${member.displayName}** has joined the server with you.`,
+		`Keep it in your pants, everyone. **${member.displayName}** just joined.`
+	];
+
+var welcomemessage = Math.floor(Math.random() * welcomes.length);
+
+const welcomeEmbed = new Discord.MessageEmbed()
+.setDescription(welcomes[welcomemessage])
+.setColor("#ffff00");
+
+try {
+	client.channels.cache.get("606901598635163708").send({ embeds: [welcomeEmbed] });
+} catch(error){
+	console.log("[ERROR]",error)}
 });
 
 
 //Send Goodbye Message
 client.on('guildMemberRemove', (member) => {
-	
-	client.users.fetch(member.id, true);
+
+client.users.fetch(member.id, true);
 
   var goodbyes = [
 	`Press F to pay respects to **${member.displayName}**.`,
-	`**${member.displayName}** has ascended.`,
 	`The wild **${member.displayName}** used teleport.`,
 	`Why did you set me on fire, **${member.displayName}**? Why didn\'t you just write your essay?`,
-	`**${member.displayName}**: Gone but not forgotten.`,
-	`**${member.displayName}** has decided to leave the server...Effective immediately.`,
 	`Ouch, **${member.displayName}** let the door hit them on the way out.`,
 	`Farewell, **${member.displayName}**.`,
+	`So long, **${member.displayName}**, and thanks for all the fish.`,
 	`**${member.displayName}** lost the game.`,
-  	`**${member.displayName}** has abandoned us in this hell hole.`,
 	`**${member.displayName}** has bailed on us.`,
-	`**${member.displayName}** vanished in a puff of smoke.`,
-	`**${member.displayName}** disappeared without a trace.`,
-	`Adieu, Adieu, to you and you and **${member.displayName}**.`,
-  	`Way to go, guys. You bullied **${member.displayName}** out of the server.`,
+  	`**${member.displayName}** vanished in a puff of smoke.`,
+ 	 `**${member.displayName}** disappeared without a trace.`,
+	`**${member.displayName}** was chewed up and spit out.`,
+	`**${member.displayName}** is outta here.`,
+ 	 `Adieu, Adieu, to you and you and **${member.displayName}**.`,
+ 	 `Way to go, guys. You bullied **${member.displayName}** out of the server.`,
 	`In the words of Fall Out Boy, thanks for the memories, **${member.displayName}**.`,
 	`**${member.displayName}** went MIA.`,
-	`**${member.displayName}** couldn\'t hang.`,
- 	`**${member.displayName}** was not the impostor. One impostor remains.`,
-	`The server will be a better place without you, **${member.displayName}**.`,
+  	`**${member.displayName}** couldn\'t hang.`,
+	`What will we do without **${member.displayName}**?`,
+	`**${member.displayName}** was not the impostor. One impostor remains.`,
 	`**${member.displayName}** has abandoned me...just like my father.`,
 	`Ding, Dong! The **${member.displayName}** is dead!`];
 
 	var goodbyemessage = Math.floor(Math.random() * goodbyes.length);
 
+	const goodbyeEmbed = new Discord.MessageEmbed()
+	.setDescription(goodbyes[goodbyemessage])
+	.setColor("#FF0000");
 
 try {
-	client.channels.cache.get("606901598635163708").send(goodbyes[goodbyemessage]);
+	client.channels.cache.get("606901598635163708").send({ embeds: [goodbyeEmbed] });
 } catch(error){
-    console.log("[ERROR]",error)}
-})
-
-//Log bans
-
-client.on('guildBanAdd', async (guild, user) => {
-
-	const LogChannel = client.channels.cache.get("632769031199522826");
-
-	const entry = await guild.fetchAuditLogs({type: 'MEMBER_BAN_ADD'}).then(audit => audit.entries.first());
-
-	if (entry.executor.id === client.user.id) return;
-
-	let embed = new Discord.MessageEmbed()
-		.setTitle("**User Banned**")
-		.setColor("#bc0000")
-	  	.setTimestamp()
-		.setDescription(stripIndents`**Banned User:** ${user.username}
-	  **Responsible Moderator:** ${entry.executor.username}`);
-
-	try {
-		LogChannel.send(embed);
-	} catch(error){
-		console.log("[ERROR]", error)
-	}
+	console.log("[ERROR]",error)}
+});
 
 
-}); 
-
-client.login(TOKEN);
+client.login(config.token);
