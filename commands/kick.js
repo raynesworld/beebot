9557d6af21636ebe62398253ce70794a79453f79
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-11-03 11:16:04 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,41 +1,41 @@
-const Discord = require("discord.js");
-const { stripIndents } = require("common-tags");
+const Discord = require('discord.js');
+const Permissions = require ('discord.js');
+const stripIndents = require("common-tags");
 const { getMember, formatDate } = require("../functions.js");
 
 module.exports = {
 	name: 'kick',
-  category: 'moderation',
+  	category: 'moderation',
 	description: 'Kicks the user from the server.',
-	usage: 'kick [user] [reason]',
+	usage: '[user] [reason]',
 	async execute(message, args) {
 
-	let user = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-	if(!user) return message.channel.send("Can't find user.");
+	let kickPerson = message.mentions.members.first() || message.guild.members.fetch(args[0]);
+	if(!kickPerson) return message.channel.send("Can't find user.");
 
 	let reason = args.slice(1).join(" ");
-  if(!reason) reason = "No reason provided.";
+  	if(!reason) reason = "No reason provided.";
 
-	if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`Nice try ${message.author.username}, but you do not have permission to kick someone.`)
-	if(user.hasPermission("KICK_MEMBERS")) return message.channel.send("That user can't be kicked.");
+	if(!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.channel.send(`Nice try ${message.author.username}, but you do not have permission to kick someone.`)
+	//if(member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.channel.send("That user can't be kicked.");
 
-	let kickEmbed = new Discord.MessageEmbed()
+	let embed = new Discord.MessageEmbed()
 	.setTitle("User Kicked")
 	.setColor("#e56b00")
 	.setTimestamp()
-  .setDescription(stripIndents`**Kicked User:** ${user.displayName}
-  **Reason:** ${reason}
-  **Responsible Moderator:** ${message.author.username}`);
+  	.setDescription(stripIndents`**Kicked User:** ${kickPerson.displayName}\n(ID: ${kickPerson.id})
+	**Reason:** ${reason}
+	**Responsible Moderator:** ${message.author.username}`);
 
-	let kickChannel = message.guild.channels.cache.find(channel => channel.name === "logs");
-	if(!kickChannel) return message.channel.send("Can't find channel.");
+	let logs = message.guild.channels.cache.get("632769031199522826");
+	if(!logs) return message.channel.send("Couldn't find logs channel");
 
   try {
-	await message.delete({timeout: 5000});
-	await user.kick(reason);
-	await kickChannel.send(kickEmbed);
-  }catch(e) {
+	await setTimeout(() => message.delete(), 10000);
+	await kickPerson.kick(reason);
+	await logs.send({ embeds: [embed] });
+  } catch(e) {
     console.error(e);
-  } //ends try/catch
-
-	},
+  }
+},
 };
