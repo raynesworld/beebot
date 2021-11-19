10f97d21c567a7a853819de83b5ef997f98359f3
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-11-19 16:07:13 -0600

    Update jail.js

--- commands/jail.js
+++ commands/jail.js
@@ -1,4 +1,5 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const Permissions = require ('discord.js');
 const ms = require("parse-ms");
 const { stripIndents } = require("common-tags");
 
@@ -6,18 +7,17 @@ module.exports = {
 	name: 'jail',
   	category: 'moderation',
 	description: 'Removes all roles from a user except the restricted prisoner role',
-	usage: '[user, reason]',
+	usage: '[user] [reason]',
 	async execute(message, args) {
 
-	let user = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+	let user = message.mentions.members.first() || await message.guild.members.fetch(args[0]);
 	if(!user) return message.channel.send("Couldn't find user.");
 
   let reason = args.slice(1).join(" ");
   if(!reason) reason = "No reason provided.";
 
-	if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(`Nice try ${message.author.username}, but you do not have permission to jail someone.`);
-
-	if(user.hasPermission("MANAGE_ROLES")) return message.channel.send(`${user} is too powerful.`);
+  if(!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.channel.send(`Nice try ${message.author.username}, but you do not have permission to jail someone.`);
+  if(user.permissions.has(Permissions.KICK_MEMBERS)) return message.channel.send('That user is too powerful.');
 
   if(message.guild === null) return message.channel.send("Please send this in a server channel, not a DM!");
 
@@ -25,11 +25,12 @@ module.exports = {
 	.setTitle(`Jailed User`)
 	.setColor("#bc0000")
 	.setTimestamp()
-	.setDescription(stripIndents`**Jailed User:** ${user}
+	.setDescription(stripIndents`**Jailed User:** ${user.displayName}
+	**ID:** ${user.id}
 	**Reason:** ${reason}
 	**Jailed By:** ${message.author.username}`);
 
-	let logs = message.guild.channels.cache.get("632769031199522826");
+	const logs = message.guild.channels.cache.get("632769031199522826");
 	if(!logs) return message.channel.send("Couldn't find logs channel.");
 
 	const roles = user.roles.cache.filter(r => r.id !== message.guild.id)
@@ -37,8 +38,8 @@ module.exports = {
 		try {
 			await user.roles.remove(roles)
 			await user.roles.add('629180728306827264')
-			await logs.send(jailEmbed)
-			await message.delete({timeout: 2000});
+			await logs.send({ embeds: [jailEmbed] })
+			await message.delete({timeout: 1000});
 		} catch(e) {
 			console.error(e);
 		}
