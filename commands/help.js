2584862aedebb293a2dd86575e9a1cceb5a336bc
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-11-19 16:15:35 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,16 @@
 const Discord = require('discord.js');
-const client = require('discord.js');
+const { Client, Intents } = require ('discord.js');
+const client = new Discord.Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES],
+	partials: ["MESSAGE", "CHANNEL", "REACTION"]
+});
 const { stripIndents } = require("common-tags");
 const { prefix } = require('../config.json');
 
 module.exports = {
 	name: 'help',
 	description: 'Displays a list of commands.',
-  aliases: ['commands'],
+  	aliases: ['commands'],
 	execute(message, args) {
 
   const data = [];
@@ -18,11 +22,11 @@ module.exports = {
 
     let helpEmbed = new Discord.MessageEmbed()
     .setTitle('Bee Bot Commands')
-    .setDescription('**Moderation:**    `ban`, `jail`, `kick`, `mute`, `warn`, `removewarn`, `purge`\n**Info:**    `help`, `serverinfo`, `warnings`, `userinfo`\n**Utilities:**    `confess`, `report`, `suggest`\n**Fun:**    `8ball`, `coinflip`, `kill`, `marco`')
-    .setFooter('Use !help [command name] for information about a specific command.')
+    .setDescription('**Info:**    `help`, `serverinfo`, `warnings`, `userinfo`\n**Moderation:**    `approve`, `ban`, `deny`, `jail`, `kick`, `mute`, `tempmute`, `warn`, `removewarn`, `purge`\n**Utilities:**    `confess`, `poll`, `qotd`, `report`, `suggest`\n**Fun:**    `8ball`, `coinflip`, `kill`, `marco`')
+    .setFooter(`Use ${prefix}help [command name] for information about a specific command.`)
     .setColor('#ffff00');
 
-    message.channel.send(helpEmbed);
+    message.channel.send({ embeds: [helpEmbed] });
       }
 
     else if(args[0])  {
@@ -33,15 +37,18 @@ module.exports = {
     data.push(`**Name:** ${command.name}`);
 
       if (!command.name) {
-			return message.reply('That is not a valid command.  Are you using an alias?');
+	return message.reply('That is not a valid command.  Are you using an alias?');
 		}
 
-   	if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+   	if (command.description) description = (`**Description:** ${command.description}`);
+		else if (!command.description) description = (`**Description** No description available`);
+		if (command.usage) usage = (`**Usage:** ${prefix}${command.name} ${command.usage}`);
+			else if (!command.usage) usage = (`**Usage:** No usage information available`);
+    	if (command.aliases) aliases = (`**Aliases:** ${command.aliases.join(', ')}`);
+			else if(!command.aliases) aliases = (`**Aliases:** None`);
 
-		message.channel.send(data, { split: true });
-
+let msg = `**Name:** ${command.name}\n${description}\n${usage}\n${aliases}`;
+message.channel.send(msg);
 }
     }
     catch(e) {
