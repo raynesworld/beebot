f33f4db4b4344405878acc901a5ee0b9bd115bf6
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-11-19 16:13:30 -0600

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,17 +1,19 @@
+const { Permissions } = require('discord.js');
+
 module.exports = {
 	name: 'purge',
-  category: 'moderation',
+  	category: 'moderation',
 	description: 'Purge up to 99 messages.',
-  usage: '[10]',
+ 	usage: '[10]',
 	execute(message, args) {
 		const amount = parseInt(args[0]) + 1;
 
 		//If user cannot delete messages
-		if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+		if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
 			return message.reply('You do not have permission to purge messages.');
 		}
 
-		//If args[0] is not a number	
+		//If args[0] is not a number
 		if (isNaN(amount)) {
 			return message.reply('that doesn\'t seem to be a valid number.');
 		} else if (amount <= 1 || amount > 100) {
