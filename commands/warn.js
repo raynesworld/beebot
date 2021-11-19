ba4286e23c01322ba73ebf230def4849686b6070
raynesworld <73146731+raynesworld@users.noreply.github.com>
2021-11-19 16:11:51 -0600

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,4 +1,10 @@
-const Discord = require('discord.js');
+const Discord = require ('discord.js');
+const { Permissions, Client, Intents } = require ('discord.js');
+const client = new Discord.Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES],
+	partials: ["MESSAGE", "CHANNEL", "REACTION"]
+});
+
 const fs = require("fs");
 const { stripIndents } = require("common-tags");
 
@@ -12,16 +18,20 @@ module.exports = {
 	async execute(message, args) {
 
 try {
-	if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You do not have permission to warn someone.");
-	let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-	if(!user) return message.reply("Can't find user.");
 
-	let wrole = message.guild.roles.cache.get("612716413098131456");
-	let reason = args.slice(1).join(" ");
-	if(!reason) return message.reply("Please provide a reason for warning this user.");
+if(!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.channel.send("You do not have permission to warn someone.");
+let user = message.mentions.members.first() || message.guild.members.cache.fetch(args[0]);
+if(!user) return message.channel.send("Can't find user.");
+
+if(message.mentions.users.id === '301212599381393409') return message.channel.send("Nice try.");
+if(user.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.channel.send('You cannot warn another moderator.');
+if(user.id === message.author.id) return message.channel.send('You cannot warn yourself.');
+
+let reason = args.slice(1).join(" ");
+if(!reason) return message.channel.send("Please provide a reason for warning this user.");
 //	let warnlevel = warnings[user.id].warnings;
 
-	if(!warnings[user.id]) warnings[user.id] = {
+if(!warnings[user.id]) warnings[user.id] = {
 		warnings: 0
 		};
 
@@ -36,22 +46,19 @@ try {
 	.setColor("#ffff00")
 	.setTimestamp()
 	.setDescription(stripIndents`**Warned User:** ${user.displayName}
+	**ID:** ${user.id}
 	**Reason:** ${reason}
 	**Warnings:** ${warnings[user.id].warnings}
 	**Warned By:** ${message.author.username}`);
 
-	let warnchannel = message.guild.channels.cache.find(channel => channel.name === "logs");
-	if(!warnchannel) return message.channel.send("Couldn't find logs channel.");
+	let logs = message.guild.channels.cache.get("632769031199522826");
+	if(!logs) return message.channel.send("Couldn't find logs channel.");
 
-	await warnchannel.send(warnEmbed);
-
-  await user.addRole(wrole.id);
-
-  await message.delete({timeout: 5000});
-
-await user.send(`You have been warned in ${message.guild.name} for ${reason}.  You have ${warnings[user.id].warnings} warnings.`);
-}catch (error) {
+	await message.delete({timeout: 1000});
+	await logs.send({ embeds: [warnEmbed] });
+	await user.send(`You have received a warning in ${message.guild.name} for ${reason}. Your number of warnings is now ${warnings[user.id].warnings}.`);
+	} catch (error) {
 	console.log(error);
 }
 }
-}
+};
